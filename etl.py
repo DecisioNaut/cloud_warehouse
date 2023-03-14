d874d7297fe9d634c9377f79f2dadcfc53ae7bf7
Caspar Heidbrink <37964284+DecisioNaut@users.noreply.github.com>
2023-03-14 13:22:47 +0100

    Format scripts using black

--- etl.py
+++ etl.py
@@ -1,5 +1,7 @@
 import configparser
+
 import psycopg2
+
 from sql_queries import copy_table_queries, insert_table_queries
 
 
@@ -21,7 +23,7 @@ def main():
 
     conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
     cur = conn.cursor()
-    
+
     load_staging_tables(cur, conn)
     insert_tables(cur, conn)
 
