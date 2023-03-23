30447994f0cfabcf69a3ba8951783fba80a3b37f
Caspar Heidbrink <37964284+DecisioNaut@users.noreply.github.com>
2023-03-23 11:36:45 +0100

    Add comments

--- create_tables.py
+++ create_tables.py
@@ -1,37 +1,48 @@
+# Imports
+# From standard library
 import configparser
 
+# From third-party libraries
 import psycopg2
 
+# From this project
 from sql_queries import create_table_queries, drop_table_queries
 
 
-def drop_tables(cur, conn):
+def drop_tables(cursor, connection):
+    """Drop all tables (staging, dimensional, and fact tables)"""
     for query in drop_table_queries:
-        cur.execute(query)
-        conn.commit()
+        cursor.execute(query)
+        connection.commit()
 
 
-def create_tables(cur, conn):
+def create_tables(cursor, connection):
+    """Create all tables (staging, dimensional, and fact tables)"""
     for query in create_table_queries:
-        cur.execute(query)
-        conn.commit()
+        cursor.execute(query)
+        connection.commit()
 
 
 def main():
+    # Read the configuration file
     config = configparser.ConfigParser()
     config.read("dwh.cfg")
 
-    conn = psycopg2.connect(
+    # Connect to the Redshift cluster and get cursor
+    connection = psycopg2.connect(
         "host={} dbname={} user={} password={} port={}".format(
             *config["CLUSTER"].values()
         )
     )
-    cur = conn.cursor()
+    cursor = connection.cursor()
 
-    drop_tables(cur, conn)
-    create_tables(cur, conn)
+    # Drop to have a clean slate
+    drop_tables(cursor, connection)
 
-    conn.close()
+    # Create tables
+    create_tables(cursor, connection)
+
+    connection.close()
 
 
 if __name__ == "__main__":
