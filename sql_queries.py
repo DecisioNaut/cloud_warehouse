8f40971a4e616c247bc7368db4685054f500ad58
Caspar Heidbrink <37964284+DecisioNaut@users.noreply.github.com>
2023-03-23 11:36:27 +0100

    Order insert queries and add comments

--- sql_queries.py
+++ sql_queries.py
@@ -6,38 +6,58 @@ config.read("dwh.cfg")
 
 # DROP TABLES
 
-staging_events_table_drop = ""
-staging_songs_table_drop = ""
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+# Staging Tables
+staging_events_table_drop = """
+"""
+
+staging_songs_table_drop = """
+"""
+
+# Dimension Tables
+time_table_drop = """
+"""
+
+user_table_drop = """
+"""
+
+artist_table_drop = """
+"""
+
+song_table_drop = """
+"""
+
+# Fact Table
+songplay_table_drop = """
+"""
 
 # CREATE TABLES
 
+# Staging Tables
 staging_events_table_create = """
 """
 
 staging_songs_table_create = """
 """
 
-songplay_table_create = """
+# Dimension Tables
+time_table_create = """
 """
 
 user_table_create = """
 """
 
-song_table_create = """
-"""
-
 artist_table_create = """
 """
 
-time_table_create = """
+song_table_create = """
 """
 
-# STAGING TABLES
+# Fact Table
+songplay_table_create = """
+"""
+
+
+# COPY DATA INTO STAGING TABLES
 
 staging_events_copy = (
     """
@@ -49,9 +69,14 @@ staging_songs_copy = (
 """
 ).format()
 
-# FINAL TABLES
+# INSERT INTO FINAL TABLES
 
-songplay_table_insert = """
+# Dimension Tables
+
+time_table_insert = """
+"""
+
+artist_table_insert = """
 """
 
 user_table_insert = """
@@ -60,37 +85,54 @@ user_table_insert = """
 song_table_insert = """
 """
 
-artist_table_insert = """
+# Fact Table
+songplay_table_insert = """
 """
 
-time_table_insert = """
-"""
 
-# QUERY LISTS
+# QUERY LISTS for import from etl.py
 
-create_table_queries = [
-    staging_events_table_create,
-    staging_songs_table_create,
-    songplay_table_create,
-    user_table_create,
-    song_table_create,
-    artist_table_create,
-    time_table_create,
-]
+# Drop Tables
 drop_table_queries = [
+    # Staging Tables
     staging_events_table_drop,
     staging_songs_table_drop,
-    songplay_table_drop,
-    user_table_drop,
-    song_table_drop,
-    artist_table_drop,
+    # Dimension Tables
     time_table_drop,
+    user_table_drop,
+    artist_table_drop,
+    song_table_drop,
+    # Fact Table
+    songplay_table_drop,
 ]
+
+# Create Tables
+create_table_queries = [
+    # Staging Tables
+    staging_events_table_create,
+    staging_songs_table_create,
+    # Dimension Tables
+    time_table_create,
+    user_table_create,
+    artist_table_create,
+    song_table_create,
+    # Fact Table
+    songplay_table_create,
+]
+
+# Copy Data into Staging Tables
 copy_table_queries = [staging_events_copy, staging_songs_copy]
+
+# Insert Data into Final Tables
+# Please note that the order of the insert queries is important
+# as the song table refernces the artist table and
+# the songplay table references all dimension tables
 insert_table_queries = [
-    songplay_table_insert,
-    user_table_insert,
-    song_table_insert,
-    artist_table_insert,
+    # Dimension Tables
     time_table_insert,
+    user_table_insert,
+    artist_table_insert,
+    song_table_insert,
+    # Fact Table
+    songplay_table_insert,
 ]
